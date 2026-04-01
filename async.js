f8091ccbcfbe39653fd23b5aa8a195c8ff962480
memesofting <bibitayoadekunle00@gmail.com>
2026-04-01 15:59:50 +0100

    async done

--- async.js
+++ async.js
@@ -7,3 +7,47 @@ let stocks = {
 
 let isShopOpen = true;
 
+ function time(ms){
+  return new Promise((resolve, reject)=>{
+    if (isShopOpen){
+      setTimeout(resolve, ms)
+    }else{
+      reject(console.log("SHop is closed"))
+    }
+  })
+ }
+
+ async function kitchen() {
+  try{
+    await time(2000)
+    console.log(`${stocks.fruits[0]} is selected`)
+
+    console.log("Start production")
+
+    await time(2000)
+    console.log("Cut the fruit")
+
+    await time(2000)
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+
+    await time(1000)
+    console.log("Starting the machine")
+
+    await time(2000)
+    console.log(`Ice cream was placed on ${stocks.holder[0]} holder`)
+
+    await time(3000)
+    console.log(`${stocks.toppings[0]} was selected as toppings`)
+    
+    await time(2000)
+    console.log("Ice cream is served!!!")
+  }
+  catch(error){
+    console.log("Customer left")
+  }
+  finally{
+    console.log("Day ended, shop closed")
+  }
+ }
+
+ kitchen()
